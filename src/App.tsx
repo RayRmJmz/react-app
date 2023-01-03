b1cc10320d73bf7c9c1aedfe9d341a57cb2f2b5d
Raymundo Ramos <raayrmjmz@gmail.com>
2023-01-03 17:08:39 -0600

    035 intro to jsx whit tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,21 @@ import './App.css';
 
 function App() {
 
-  const [fecha, setFecha] = useState(new Date())
-  
-  useEffect(() => {
-    const timerId = setInterval(() =>{
-      setFecha(new Date())
-    },1000)
+  const subtitlo = "Esto es un subtitulo";
 
-    return () => clearInterval(timerId)
-  })
+  const duplicar = (valor : number) => valor * 2 ;
 
+  const imgURL = "https://cdn.pixabay.com/photo/2022/12/26/19/57/champagne-7679665_960_720.jpg"
   return (
-    <div className="App">
-      <div className="App">
-      <h3>Ejemplo</h3>
-      <input type="text" />
-      <div>{fecha.toString()}</div>
-    </div>
-    </div>
+    <>
+      <h1>Hola mundo!</h1>
+      <h3>{subtitlo.toUpperCase()}</h3>
+      <h4>El doble de 3 es {duplicar(3)}</h4>
+      <div>
+        <input type="checkbox" checked={true} /> Esto es un checkbox
+      </div>
+      <img src={imgURL} alt="img muestra" />
+    </>
   );
 }
 
