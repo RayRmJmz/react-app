f78aa2e31356cff413ac1333463d193c335e5ba5
Raymundo Ramos <raayrmjmz@gmail.com>
2023-01-03 17:22:30 -0600

    036 css in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import AppCSS from './App.module.css'
 function App() {
 
   const subtitlo = "Esto es un subtitulo";
 
   const duplicar = (valor : number) => valor * 2 ;
 
-  const imgURL = "https://cdn.pixabay.com/photo/2022/12/26/19/57/champagne-7679665_960_720.jpg"
+  const imgURL = "https://cdn.pixabay.com/photo/2022/12/26/19/57/champagne-7679665_960_720.jpg";
+
+  const cuadradoAzul = {
+    backgroundColor : 'blue', 
+    width : '50px', 
+    height: '50px', 
+    marginLeft: '1rem'
+  }
   return (
     <>
-      <h1>Hola mundo!</h1>
-      <h3>{subtitlo.toUpperCase()}</h3>
-      <h4>El doble de 3 es {duplicar(3)}</h4>
+      <h1 className='rojo'>Hola mundo!</h1>
+      <div className='cuadradoRojo'></div>
+      <div style={{backgroundColor : 'green', width : '50px', height: '50px', marginLeft: '1rem'}}></div>
+      <div style={cuadradoAzul}></div>
+      <h3 style={{color: 'blue'}}>{subtitlo.toUpperCase()}</h3>
+      
+      <h4 className='color'>Index css El doble de 3 es {duplicar(3)}</h4>
+      <h4 className={AppCSS.color}>App.module css El doble de 3 es {duplicar(3)}</h4>
       <div>
         <input type="checkbox" checked={true} /> Esto es un checkbox
       </div>
