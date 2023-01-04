a6c16bcd03a865c5933c5f83f42daa72e2f4a45e
Raymundo Ramos <raayrmjmz@gmail.com>
2023-01-04 16:42:03 -0600

    071 basic form  Formik

--- src/generos/CrearGenero.tsx
+++ src/generos/CrearGenero.tsx
@@ -1,6 +1,8 @@
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import Button from "../utils/Button"
 import ROUTES from '../route-config'
+import { Field, Form, Formik } from "formik"
+
 
 export default function CrearGenero(){
 
@@ -9,7 +11,27 @@ export default function CrearGenero(){
     return (
         <>
         <h3>Crear Género</h3>
-        <Button onClick={() => navigate(ROUTES.GENEROS)}>Guardar</Button>
+
+        <Formik 
+        initialValues={{nombre : ''}}
+        onSubmit={values => console.log(values)}
+        >
+            <Form>
+
+                <div className="form-floating mb-3">
+                    <Field name='nombre' className="form-control"  id="nombreLabel" placeholder="Nombre"/>
+                    <label htmlFor="nombreLabel">Nombre</label>
+                </div>
+
+                <div className="d-grid gap-2 d-sm-flex">
+                    <Button type="submit">Guardar</Button>
+                    <Link to={ROUTES.GENEROS} className="btn btn-dark">Regresar</Link>
+                </div>
+                
+            </Form>
+            
+        </Formik>
+
         </>
         
     )
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from "react"
 export default function Button( props : buttonProps){
 
     return (
-        <button type="button" className="btn btn-primary" onClick={props.onClick}>
+        <button type={props.type} className="btn btn-primary" onClick={props.onClick}>
             {props.children}
         </button>
     )
@@ -12,5 +12,11 @@ export default function Button( props : buttonProps){
 
 interface buttonProps {
     children : React.ReactNode,
-    onClick(): void
+    onClick?(): void,
+    type : "button" | "submit"
+
+}
+
+Button.defaultProps = {
+    type: "button"
 }
