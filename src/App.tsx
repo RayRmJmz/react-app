0d32e49148749c3d57bee2164d031aa76fe9b818
Raymundo Ramos <raayrmjmz@gmail.com>
2023-01-04 10:02:14 -0600

    054 pelicula individual

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,20 @@
-import React, { HtmlHTMLAttributes, useEffect, useState } from 'react';
-import './App.css';
-import MostrarTexto from './MostrarTexto';
-import ProyectarContenido from './ProyectarContenido';
-import ProyectarContenido2 from './ProyectarContenido2';
-import EjemploReloj from './EjemploReloj';
-import ContenidoDinamico from './ContenidoDinamico';
-import FormularioTexto from './FormularioTexto';
-import EjemploUseEffect from './EjemploUseEffect';
-import ValorContext from './ValorContext';
-import Abuelo from './Abuelo';
+import './App.css'
+import PeliculaIndividual from './peliculas/PeliculaIndividual';
+import { pelicula } from './peliculas/Peliculas.model';
+
 
 function App() {
 
-  //let texto = ''
-  const [texto, setTexto] = useState('')
+  const peliculaPrueba : pelicula = {
+    id : 1,
+    titulo : 'Lord of the rings : the fellowship of ring',
+    poster : 'https://imgs.search.brave.com/5cNzZj74YgWaRXQcDpNTRzLSaYBUtz6Nd3ct76hIm6I/rs:fit:312:225:1/g:ce/aHR0cHM6Ly90c2Ux/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5M/MnhQRlgwcDU1UzRP/eWRob3B0MXR3SGFM/USZwaWQ9QXBp'
 
-  const [checked, setChecked] = useState(true)
-
-  const manejarClick = () => console.log('click')
-
-  const manejarKeyUpApp = ( textInput: string) => {
-    console.log(textInput)
-    //texto = e.currentTarget.value
-    setTexto(textInput)
   }
 
-  const parteIntermediaConst = <EjemploReloj />
-
-  const estilo = {
-    backgroundColor : 'red',
-    width : '50px',
-    height : '50px',
-    marginLeft : '1rem'
-  }
-
-  const calificaciones = [
-    {nombre : 'Felipe', calificacion: 75},
-    {nombre : 'Claudia', calificacion: 85},
-    {nombre : 'Roberto', calificacion: 95},
-  ]
-
-  const parteInferiorConst = <div style={estilo}></div>
   return (
     <>
-      <div>
-        <input type="checkbox" 
-        onChange={ (e) => setChecked(e.currentTarget.checked)}
-        checked={checked} /> Mostrar componente use effect
-      </div>
-
-      <ValorContext.Provider value={texto}>
-        <Abuelo />
-      </ValorContext.Provider>
-
-      {checked? <EjemploUseEffect /> : null }
-      <button onClick={manejarClick}>Click me</button>
-      <br />
-      <button onClick={() => console.log('click 2')}>Click me 2</button>
-      <br />
-      <ContenidoDinamico calificacion = {95} />
-      <ContenidoDinamico calificacion = {65} />
-
-      {calificaciones.map(cal => <ContenidoDinamico key = {cal.nombre} {...cal} />)}
-      <ProyectarContenido>
-        <>
-        <h6>Dentro del contenido</h6>
-        <span>Proyectando contenido</span>
-        </>
-      </ProyectarContenido>
-
-      <ProyectarContenido2 
-      //parteSuperior = {<h3>Esta es la parte superios de contenido 2 </h3> }
-      parteIntermedia = {parteIntermediaConst}
-      parteInferior = {parteInferiorConst}
-      />
-
-      <FormularioTexto manejarKeyUp={(e: string) => manejarKeyUpApp(e)} /> 
-
-      <MostrarTexto texto = {texto} />
-
-      <MostrarTexto />
-      <div>
-            <input type="checkbox" 
-            onChange={ (e) => setChecked(e.currentTarget.checked)}
-            checked={checked} /> Esto es un checkbox
-      </div>
+    <PeliculaIndividual pelicula={peliculaPrueba}/>
     </>
   );
 
