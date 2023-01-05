35bb4869b45846c43df9b96d965f40a07658b1ae
Raymundo Ramos <raayrmjmz@gmail.com>
2023-01-05 10:04:00 -0600

    074 desactivar btn

--- src/generos/CrearGenero.tsx
+++ src/generos/CrearGenero.tsx
@@ -1,40 +1,48 @@
-import { Link, useNavigate } from "react-router-dom"
-import Button from "../utils/Button"
-import ROUTES from '../route-config'
-import { Form, Formik } from "formik"
-import * as Yup from 'yup'
-import FormGroupText from "../utils/FormGroupText"
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../utils/Button";
+import ROUTES from "../route-config";
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import FormGroupText from "../utils/FormGroupText";
 
+export default function CrearGenero() {
+  return (
+    <>
+      <div className="container">
+        <h3>Crear Género</h3>
 
-export default function CrearGenero(){
+        <Formik
+          initialValues={{ nombre: "" }}
+          onSubmit={async (values) => {
+            await new Promise((r) => setTimeout(r, 100));
+            console.log(values);
+          }}
+          validationSchema={Yup.object({
+            nombre: Yup.string()
+              .required("* Este campo es requerido")
+              .primeraLetraMayuscula(),
+          })}
+        >
+          {(formikProps) => (
+            <Form>
+              <FormGroupText
+                campo="nombre"
+                label="Nombre"
+                placeholder="Nombre"
+              />
 
-    return (
-        <>
-        <div className="container">
-            <h3>Crear Género</h3>
-
-            <Formik 
-            initialValues={{nombre : ''}}
-            onSubmit={values => console.log(values)}
-
-            validationSchema={Yup.object({
-                nombre : Yup.string().required('* Este campo es requerido').primeraLetraMayuscula()
-            })}
-            >
-                <Form>
-
-                    <FormGroupText campo="nombre" label="Nombre" placeholder="Nombre"/>
-
-                    <div className="d-grid gap-2 d-sm-flex">
-                        <Button type="submit">Guardar</Button>
-                        <Link to={ROUTES.GENEROS} className="btn btn-dark">Regresar</Link>
-                    </div>
-                    
-                </Form>
-                
-            </Formik>
-        </div>
-        </>
-        
-    )
+              <div className="d-grid gap-2 d-sm-flex">
+                <Button disabled={formikProps.isSubmitting} type="submit">
+                  Guardar
+                </Button>
+                <Link to={ROUTES.GENEROS} className="btn btn-dark">
+                  Regresar
+                </Link>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </>
+  );
 }
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -1,20 +1,24 @@
-export default function Button( props : buttonProps){
-
-    return (
-        <button type={props.type} className="btn btn-primary" onClick={props.onClick}>
-            {props.children}
-        </button>
-    )
-
+export default function Button(props: buttonProps) {
+  return (
+    <button
+      type={props.type}
+      disabled={props.disabled}
+      className="btn btn-primary"
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 interface buttonProps {
-    children : React.ReactNode,
-    onClick?(): void,
-    type : "button" | "submit"
-
+  children: React.ReactNode;
+  onClick?(): void;
+  type: "button" | "submit";
+  disabled: boolean;
 }
 
 Button.defaultProps = {
-    type: "button"
-}
+  type: "button",
+  disabled: false,
+};
